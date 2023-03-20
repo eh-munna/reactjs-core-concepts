9183ccc82586662a25032dad0c60d98877159bf3
ehmunna <emran-hussain.munna-tu-ilmenau.de>
2023-03-20 20:50:08 +0100

    practice react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
 
+const demoObj = {
+  playerName: 'AB de Villiers',
+  age: 43,
+};
+
+const elementStyle = {
+  color: 'aquamarine',
+};
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>JSX</h1>
+        {/*  using className  */}
+        <div className="container">
+          <h4>Hello ClassName</h4>
+          {/* now we add style with className */}
+        </div>
+
+        {/* we can run any valid JS inside block like template string*/}
+        <div className="add">
+          {/* inside the curly braces we can write js code */}
+          <p>{100 + 30}</p>
+          {/* accessing an object property also setting color from another object */}
+          <p style={elementStyle}>
+            Player Name : {demoObj.playerName}, and {demoObj.age} years old
+          </p>
+          {/* inline style */}
+          <p style={{ color: 'slateBlue', backgroundColor: 'aquamarine' }}>
+            Player Name : {demoObj.playerName}, and {demoObj.age} years old
+          </p>
+        </div>
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
